96213aca41641a8c9cedd3b31ab415a2faf9cb81
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-10 23:32:39 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4. Task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4.Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum + Math.Pow((2 * i - 1), 2);
+
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+
+}
